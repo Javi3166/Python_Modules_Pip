9fb5be649cb0e9b7f482516b2597f6b7ea1bdbbd
Emilio <javi3166@yahoo.com>
2024-11-01 15:36:22 -0400

    Initial commit and final code

---
+++ Main.py
@@ -0,0 +1,16 @@
+import Useful_Tools
+
+print(Useful_Tools.roll_dice(10))
+
+"""
+PyCharm already comes with a lot of other modules. It is possible to get external modules developed by other people. 
+Just need to Google them. Once I find one I like, I just need to open the PyCharm Terminal and type:
+
+pip install MODULE_NAME
+
+Then I can use it easily with the import function
+
+If I want to uninstall a module I just need to type:
+
+pip uninstall MODULE_NAME
+"""
